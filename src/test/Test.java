842169e64c49a3a71a0e0c2362502171a9d935a5
Vimukthi Lakshan <81724467+vimukthilakshan11@users.noreply.github.com>
2023-08-18 17:25:16 +0530

    yes

---
+++ src/test/Test.java
@@ -0,0 +1,32 @@
+
+package test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import v.DB.DB;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class Test {
+public static void main(String[] args) throws SQLException, Exception {
+ Connection con = DB.getDBConnection();
+ if(con != null){
+     System.out.println("Success");
+ }
+    
+    
+    ResultSet rs = con.createStatement().executeQuery("SELECT * FROM Customer"); 
+    while (rs.next()){
+        System.out.println(rs.getString("LName"));
+    }
+    
+}
+
+}
